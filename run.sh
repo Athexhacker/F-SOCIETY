920452db456e92cd03cf6efd56bb1af68f6772cf
A T H E X <athexhacker@gmail.com>
2025-10-24 11:40:14 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -899,7 +899,7 @@ echo -e "\e[1;35m https://instagram.com/itx_inzi86 \e[0m"
             echo "<----------------------------menu-------------------------->"
             echo "1) Location finder"
             echo "2) Camera Hacking"
-            echo "3) Android Hacking BY QR code"
+            echo "3) Android Hacking"
             echo "4) WEBSITE Pentesting"
             echo "5) SN1PER TOOLKIT"
             echo "6) Simple Ransomware"
@@ -914,6 +914,32 @@ echo -e "\e[1;35m https://instagram.com/itx_inzi86 \e[0m"
 			echo "15) Change ip every second"
 			echo "16) E-mail-brute-Force"
 			echo "17) F-SOCIETY-DDOS"
+			echo "18) ATHEX-SPY"
+			echo "19) HUNTER-V2"
+			echo "20) HUNTER"
+			echo "21) BT-DOS"
+			echo "22) REDOX"
+			echo "23) py-ddoser"
+			echo "24) DDOS"
+			echo "25) blackshark"
+			echo "26) EXPLOITER"
+			echo "27) Whatsapp-Crasher"
+			echo "28) X-SERVER"
+			echo "29) InstaReporter"
+			echo "30) cracker"
+			echo "31) insta-bf"
+			echo "32) facebook-bruteforce"
+			echo "33) E-Mail-Bulker"
+			echo "34) SMS-BOMBER"
+			echo "35) SpyEar"
+			echo "36) QR-PHISH"
+			echo "37) ANDRO-EYE"
+			echo "38) MailFinder"
+			echo "39) Telegram-Mass-report"
+			echo "40) TT-mass-report"
+			echo "41) wifi-fucker"
+			echo "42) Black-Phish"
+			echo "43) ATHEXSQL"
 			echo "A) install All Tools In one Click"
             echo "<-------------------------------------------------------------->"
             read -p "Enter an option to continue: " ochoice
@@ -1038,6 +1064,187 @@ echo -e "\e[1;35m https://instagram.com/itx_inzi86 \e[0m"
                             echo "<-----------------------Installing F-SOCIETY-DDOS------------------------->"
                             git clone https://github.com/Athexhacker/F-SOCIETY-DDOS
                             ;;    	
+					 18)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing ATHEX-SPY------------------------->"
+                            git clone https://github.com/Athexhacker/ATHEX-SPY
+                            ;;    	
+					 19)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing HUNTER-V2------------------------->"
+                            git clone https://github.com/Athexhacker/HUNTER-V2
+                            ;;    	
+					 20)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing HUNTER------------------------->"
+                            git clone https://github.com/Athexhacker/hunter
+                            ;; 
+					 21)  	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing BT-DOS------------------------->"
+                            git clone https://github.com/Athexhacker/BT-DOS
+                            ;;  
+					 22)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing REDOX------------------------->"
+                            git clone https://github.com/Athexhacker/REDOX
+                            ;;   
+					 23)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing PY-DDOSER------------------------->"
+                            git clone https://github.com/Athexhacker/py-ddoser
+                            ;;    
+					 24)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing DDOS------------------------->"
+                            git clone https://github.com/Athexhacker/F-SOCIETY-DDOS
+                            ;; 
+					 25)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing BLACKSHARK------------------------->"
+                            git clone https://github.com/Athexhacker/blackshark
+                            ;;  
+					 26)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing EXPLOITER------------------------->"
+                            git clone https://github.com/Athexhacker/EXPLOITER
+                            ;;    
+					 27)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing WHATSAPP-CRASHER------------------------->"
+                            git clone https://github.com/Athexhacker/Whatsapp-Crasher
+                            ;;    
+					 28)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing X-SERVER------------------------->"
+                            git clone https://github.com/Athexhacker/X-SERVER
+                            ;;   
+					 29)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing INSTA-REPORTING------------------------->"
+                            git clone https://github.com/Athexhacker/InstaReporter
+                            ;;   
+					 30)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing CRACKER------------------------->"
+                            git clone https://github.com/Athexhacker/cracker
+                            ;; 
+					 31)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing INSTA-BRUTE-FORCE------------------------->"
+                            git clone https://github.com/Athexhacker/insta-bf
+                            ;;
+					 32)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing FB-BRUTE FORCE------------------------->"
+                            git clone https://github.com/Athexhacker/facebook-bruteforce
+                            ;;
+					 33)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing E-MAIL BULKER------------------------->"
+                            git clone https://github.com/Athexhacker/E-Mail-Bulker
+                            ;;  
+					 34)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing SMS-BOMBER------------------------->"
+                            git clone https://github.com/Athexhacker/SMS-BOMBER
+                            ;;
+					 35)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing SPY-EAR------------------------->"
+                            git clone https://github.com/Athexhacker/SpyEar
+                            ;; 
+					 36)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing QR-PHISHING------------------------->"
+                            git clone https://github.com/Athexhacker/QR-PHISH
+                            ;;
+					 37)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing ANDRO-EYE------------------------->"
+                            git clone https://github.com/Athexhacker/ANDRO-EYE
+                            ;; 
+					 38)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing MAIL-FINDER------------------------->"
+                            git clone https://github.com/Athexhacker/MailFinder
+                            ;; 
+					 39)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing TELEGRAM-MASS-REPORTER------------------------->"
+                            git clone https://github.com/Athexhacker/Telegram-Mass-report
+                            ;; 
+					 40)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing TIKTOK-MASS REPORTING------------------------->"
+                            git clone https://github.com/Athexhacker/TT-mass-report
+                            ;; 
+					41)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing WIFI-FUCKER------------------------->"
+                            git clone https://github.com/Athexhacker/wifi-fucker
+                            ;; 
+					42)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing BLACKPHISH------------------------->"
+                            git clone https://github.com/Athexhacker/Black-Phish
+                            ;;   
+					43)	
+                            clear
+                    	    figlet -c "ATHEX BLACK HAT"
+                            echo "<-------------------------------------------------------------------->"
+                            echo "<-----------------------Installing ATHEX-SQL------------------------->"
+                            git clone https://github.com/Athexhacker/ATHEXSQL		   		   			   			   			   			   			   			    			  			    			    			   			 			 									  			   						 			  				
                     A)      
                             clear
                     	    figlet -c "ATHEX BLACK HAT"
@@ -1060,6 +1267,33 @@ echo -e "\e[1;35m https://instagram.com/itx_inzi86 \e[0m"
                             git clone https://github.com/Athexhacker/WI-SNIPER
                             git clone https://github.com/Athexhacker/athex-ransomware
                             git clone https://github.com/Athexhacker/inzi-DDOS
+							git clone https://github.com/Athexhacker/ATHEX-SPY
+                            git clone https://github.com/Athexhacker/ATHEXSQL
+                            git clone https://github.com/Athexhacker/HUNTER-V2
+                            git clone https://github.com/Athexhacker/hunter
+                            git clone https://github.com/Athexhacker/BT-DOS
+                            git clone https://github.com/Athexhacker/REDOX
+                            git clone https://github.com/Athexhacker/py-ddoser
+                            git clone https://github.com/Athexhacker/F-SOCIETY-DDOS
+                            git clone https://github.com/Athexhacker/blackshark
+                            git clone https://github.com/Athexhacker/EXPLOITER
+                            git clone https://github.com/Athexhacker/Whatsapp-Crasher
+                            git clone https://github.com/Athexhacker/X-SERVER
+                            git clone https://github.com/Athexhacker/InstaReporter
+                            git clone https://github.com/Athexhacker/cracker
+                            git clone https://github.com/Athexhacker/insta-bf
+                            git clone https://github.com/Athexhacker/facebook-bruteforce
+                            git clone https://github.com/Athexhacker/E-Mail-Bulker
+                            git clone https://github.com/Athexhacker/SMS-BOMBER
+                            git clone https://github.com/Athexhacker/SpyEar
+                            git clone https://github.com/Athexhacker/QR-PHISH
+                            git clone https://github.com/Athexhacker/ANDRO-EYE
+                            git clone https://github.com/Athexhacker/MailFinder
+                            git clone https://github.com/Athexhacker/Telegram-Mass-report
+                            git clone https://github.com/Athexhacker/TT-mass-report
+                            git clone https://github.com/Athexhacker/wifi-fucker
+                            git clone https://github.com/Athexhacker/Black-Phish
+                            git clone https://github.com/Athexhacker/ATHEXSQL
                             ;;     
                     B)     
                             bash run.sh
@@ -1147,6 +1381,33 @@ git clone https://github.com/Athexhacker/DDOS
 git clone https://github.com/Athexhacker/WI-SNIPER
 git clone https://github.com/Athexhacker/athex-ransomware
 git clone https://github.com/Athexhacker/inzi-DDOS
+git clone https://github.com/Athexhacker/ATHEX-SPY
+git clone https://github.com/Athexhacker/ATHEXSQL
+git clone https://github.com/Athexhacker/HUNTER-V2
+git clone https://github.com/Athexhacker/hunter
+git clone https://github.com/Athexhacker/BT-DOS
+git clone https://github.com/Athexhacker/REDOX
+git clone https://github.com/Athexhacker/py-ddoser
+git clone https://github.com/Athexhacker/F-SOCIETY-DDOS
+git clone https://github.com/Athexhacker/blackshark
+git clone https://github.com/Athexhacker/EXPLOITER
+git clone https://github.com/Athexhacker/Whatsapp-Crasher
+git clone https://github.com/Athexhacker/X-SERVER
+git clone https://github.com/Athexhacker/InstaReporter
+git clone https://github.com/Athexhacker/cracker
+git clone https://github.com/Athexhacker/insta-bf
+git clone https://github.com/Athexhacker/facebook-bruteforce
+git clone https://github.com/Athexhacker/E-Mail-Bulker
+git clone https://github.com/Athexhacker/SMS-BOMBER
+git clone https://github.com/Athexhacker/SpyEar
+git clone https://github.com/Athexhacker/QR-PHISH
+git clone https://github.com/Athexhacker/ANDRO-EYE
+git clone https://github.com/Athexhacker/MailFinder
+git clone https://github.com/Athexhacker/Telegram-Mass-report
+git clone https://github.com/Athexhacker/TT-mass-report
+git clone https://github.com/Athexhacker/wifi-fucker
+git clone https://github.com/Athexhacker/Black-Phish
+git clone https://github.com/Athexhacker/ATHEXSQL
 ;;
 
 X)
